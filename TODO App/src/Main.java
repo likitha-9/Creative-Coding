6892d0df54cf71bc012656f0a41de6e4eb492d59
likitha9 <sivalikitha.valluru9@gmail.com>
2020-05-18 13:38:58 -0400

    Update Main.java
    edits:
    - borderpane->flowpane
    - adding UI elements

--- TODO App/src/Main.java
+++ TODO App/src/Main.java
@@ -1,24 +1,41 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	static BorderPane border = new BorderPane();
-	Button addEvent = new Button();
+	static FlowPane flow = new FlowPane();
+	Button addEvent = new Button("Add Event"), delEvent = new Button("Delete Event"),
+			editEvent = new Button("Edit Event");
 
 	@Override
 	public void start(Stage mainStage) throws Exception {
-		Scene scene = new Scene(border, 300, 500);
-		
+		Scene scene = new Scene(flow, 280, 500);
+
+		flow.getChildren().add(addButtons());
+
+		addEvent.setOnMouseClicked(e -> {
+			AddEvent.addEvent();
+		});
+
 		mainStage.setTitle("To-Do List");
 		mainStage.setScene(scene);
-		mainStage.show();		
+		mainStage.setResizable(false);// disables maximizing of window
+		mainStage.show();
+	}
+
+	public HBox addButtons() {
+		HBox hbox = new HBox(10); // sets spacing=10
+		hbox.setPadding(new Insets(20));
+		hbox.getChildren().addAll(addEvent, delEvent, editEvent);
+		return hbox;
 	}
 
 	public static void main(String[] args) {
-		Application.launch();
+		Application.launch(args);
 	}
 }

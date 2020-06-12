892cf677d84355119d70c0d51f11e4e1ac795706
likitha9 <sivalikitha.valluru9@gmail.com>
2020-06-12 12:25:00 -0400

    Update Main.java

--- Cipher/src/application/Main.java
+++ Cipher/src/application/Main.java
@@ -1,25 +1,47 @@
 package application;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 	@Override
 	public void start(Stage mainStage) {
-		//Create HBox
-		HBox hbox=new HBox(20);
-		//add children to that HBox
-		Label enterLabel = new Label("Enter the text to encode:\t");
-		TextField enterText=new TextField("Enter text.");
-		hbox.getChildren().addAll(enterLabel,enterText);
+		// Create VBox (MAIN)
+		VBox vbox=new VBox(20);
 
-		//Create scene and add it to stage.
-		Scene scene = new Scene(hbox, 40, 400);
+		// Enable writing text
+		HBox hbox_writing = new HBox(20);
+		Label enterLabel = new Label("Enter the text to encode:\t");
+		TextField enterText = new TextField("Enter text.");
+		hbox_writing.getChildren().addAll(enterLabel, enterText);
+
+		// Create a choice box for the ciphers.
+		HBox hbox_choices=new HBox(20);
+		Label choiceLabel = new Label("Select a cipher:\t");
+		ChoiceBox choices = new ChoiceBox<String>(FXCollections.observableArrayList(
+				"Atbash",
+				"ROT13",
+				"Caesar",
+				"Affine",
+				"Rail-Fence",
+				"Baconian",
+				"Polybius Square"
+				));
+		hbox_choices.getChildren().addAll(choiceLabel,choices);
+
+		// Add all children to the main VBox.
+		vbox.getChildren().addAll(hbox_writing, hbox_choices);
+
+		// Create scene and add it to stage.
+		Scene scene = new Scene(vbox, 400, 400);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		mainStage.setScene(scene);
 		mainStage.show();

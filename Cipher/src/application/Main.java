1f822e2e62a62930936fe85ab6523d1e8036e6ca
likitha9 <sivalikitha.valluru9@gmail.com>
2020-06-12 15:40:11 -0400

    FXML

--- Cipher/src/application/Main.java
+++ Cipher/src/application/Main.java
@@ -1,7 +1,11 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.collections.FXCollections;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
@@ -13,9 +17,9 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
 	@Override
-	public void start(Stage mainStage) {
+	public void start(Stage mainStage) throws IOException {
 		// Create VBox (MAIN)
-		VBox vbox=new VBox(20);
+		VBox vbox = new VBox(20);
 
 		// Enable writing text
 		HBox hbox_writing = new HBox(20);
@@ -24,45 +28,25 @@ public class Main extends Application {
 		hbox_writing.getChildren().addAll(enterLabel, enterText);
 
 		// Create a choice box for the ciphers.
-		HBox hbox_choices=new HBox(20);
+		HBox hbox_choices = new HBox(20);
 		Label choiceLabel = new Label("Select a cipher:\t");
-		ChoiceBox choices = new ChoiceBox<String>(FXCollections.observableArrayList(
-				"Atbash",
-				"ROT13",
-				"Caesar",
-				"Affine",
-				"Rail-Fence",
-				"Baconian",
-				"Polybius Square",
-				"Simple Substitution",
-				"Codes and Nomenclators",
-				"Columnar Transposition",
-				"Autokey",
-				"Beaufort",
-				"Porta",
-				"Running Key",
-				"Vigenère and Gronsfeld",
-				"Homophonic Substitution",
-				"Four-Square",
-				"Hill",
-				"Playfair",
-				"ADFGVX",
-				"ADFGX",
-				"Bifid",
-				"Straddle Checkerboard",
-				"Trifid",
-				"Base64",
-				"Fractionated Morse"
-				));
-		hbox_choices.getChildren().addAll(choiceLabel,choices);
+		ChoiceBox<String> choices = new ChoiceBox<String>(FXCollections.observableArrayList("Atbash", "ROT13", "Caesar",
+				"Affine", "Rail-Fence", "Baconian", "Polybius Square", "Simple Substitution", "Codes and Nomenclators",
+				"Columnar Transposition", "Autokey", "Beaufort", "Porta", "Running Key", "Vigenère and Gronsfeld",
+				"Homophonic Substitution", "Four-Square", "Hill", "Playfair", "ADFGVX", "ADFGX", "Bifid",
+				"Straddle Checkerboard", "Trifid", "Base64", "Fractionated Morse"));
+		hbox_choices.getChildren().addAll(choiceLabel, choices);
 
 		// Add all children to the main VBox.
 		vbox.getChildren().addAll(hbox_writing, hbox_choices);
 
-		//
+
+		Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+
+		Scene scene = new Scene(root);
 
 		// Create scene, add it to stage, and show the stage.
-		Scene scene = new Scene(vbox, 400, 400);
+		//Scene scene = new Scene(vbox, 400, 400);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		mainStage.setScene(scene);
 		mainStage.show();

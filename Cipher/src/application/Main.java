946314429bcfdf9846655c3597c8304cad6c837b
likitha9 <sivalikitha.valluru9@gmail.com>
2020-06-12 12:13:46 -0400

    Update Main.java

--- Cipher/src/application/Main.java
+++ Cipher/src/application/Main.java
@@ -1,25 +1,30 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class Main extends Application {
+
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage mainStage) {
+		//Create HBox
+		HBox hbox=new HBox(20);
+		//add children to that HBox
+		Label enterLabel = new Label("Enter the text to encode:\t");
+		TextField enterText=new TextField("Enter text.");
+		hbox.getChildren().addAll(enterLabel,enterText);
+
+		//Create scene and add it to stage.
+		Scene scene = new Scene(hbox, 40, 400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		mainStage.setScene(scene);
+		mainStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}

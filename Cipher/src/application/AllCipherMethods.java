044b1f3a456ac3ffb047a22590872fb5bec09f39
likitha9 <sivalikitha.valluru9@gmail.com>
2020-06-12 20:42:24 -0400

    Create AllCipherMethods.java

---
+++ Cipher/src/application/AllCipherMethods.java
@@ -0,0 +1,107 @@
+package application;
+
+public class AllCipherMethods {
+	public void atbash() {
+
+	}
+
+	public void rot13() {
+
+	}
+
+	public void caesar() {
+
+	}
+
+	public void affine() {
+
+	}
+
+	public void railFence() {
+
+	}
+
+	public void baconian() {
+
+	}
+
+	public void polybiusSquare() {
+
+	}
+
+	public void simpleSubstitution() {
+
+	}
+
+	public void codesAndNomenclators(){
+
+	}
+
+	public void columnarTransposition() {
+
+	}
+
+	public void autokey() {
+
+	}
+
+	public void beaufort() {
+
+	}
+
+	public void porta() {
+
+	}
+
+	public void runningKey() {
+
+	}
+
+	public void vigenereAndGronsfeld() {
+
+	}
+
+	public void homophonicSubstitution() {
+
+	}
+
+	public void fourSquare() {
+
+	}
+
+	public void hill() {
+
+	}
+
+	public void playfair() {
+
+	}
+
+	public void adfgvx() {
+
+	}
+
+	public void adfgx() {
+
+	}
+
+	public void bifid() {
+
+	}
+
+	public void straddleCheckerboard() {
+
+	}
+
+	public void trifid() {
+
+	}
+
+	public void base64() {
+
+	}
+
+	public void fractionatedMorse() {
+
+	}
+}
